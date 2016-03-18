94a963b5d3187e1e682072c647b6c05addc2d9a6
Gonzalo Vallerino <Gonzalo Vallerino>
2016-03-18 02:00:04 -0300

    Armando estructura base. Se dejan metodos predefinidos sin usar y metodos mas complejos comentados para futura implementacion

--- src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
+++ src/main/java/ar/fiuba/tdd/template/tp0/RegExGenerator.java
@@ -8,6 +8,16 @@ public class RegExGenerator {
 
     private int maxLength;
 
+    private static final String POINT = ".";
+    private static final String SQUARE_BRACKET_OPEN = "[";
+    private static final String SQUARE_BRACKET_CLOSE = "]";
+    private static final String BACKSLASH = "\\";
+
+    private static final String ASTERISK = "*";
+    private static final String PLUS = "+";
+    private static final String QUESTION_MARK = "?";
+
+
     public RegExGenerator(int maxLength) {
 
         this.maxLength = maxLength;
@@ -15,9 +25,12 @@ public class RegExGenerator {
 
     public List<String> generate(String regEx, int numberOfResults) {
 
+//      Map<String, List<String>> mapRegEx = this.generateMapsOfRegEx(regEx);
         List<String> regExList = new ArrayList<>();
 
         for (int i = 0; i < numberOfResults; i++) {
+
+//          String regExGenerated = this.generateToken(mapRegEx);
             String regExGenerated = this.generateToken(regEx);
             regExList.add(regExGenerated);
         }
@@ -34,4 +47,34 @@ public class RegExGenerator {
         return Character.toString(regEx.charAt(random));
     }
 
+    private String getNextCharacter(String regEx, int index) {
+
+        int nextIndex = index + 1;
+        if (nextIndex >= regEx.length()) {
+            return "";
+        }
+        return Character.toString(regEx.charAt(nextIndex));
+    }
+
+    private Boolean characterIsQuantifier(String character) {
+        Boolean isAsterisk = (ASTERISK.equals(character));
+        Boolean isPlus = (PLUS.equals(character));
+        Boolean isQuestionMark = (QUESTION_MARK.equals(character));
+        return (isAsterisk || isPlus || isQuestionMark);
+    }
+
+    private Boolean characterIsSpecial(String character) {
+        Boolean isPoint = POINT.equals(character);
+        Boolean isSquareOpen = SQUARE_BRACKET_OPEN.equals(character);
+        Boolean isSquareClose = SQUARE_BRACKET_CLOSE.equals(character);
+        Boolean isBacklash = BACKSLASH.equals(character);
+        return (isPoint || isSquareOpen || isSquareClose || isBacklash);
+    }
+
+    /*
+    private Map<String,List<String>> generateMapsOfRegEx(String regEx);
+
+    private String generateToken(Map<String,List<String>> mapRegEx);
+
+     */
 }
