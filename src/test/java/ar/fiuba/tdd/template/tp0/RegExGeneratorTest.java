df43a0d41c51294074c8ddea7712e2a03b78bc26
Gonzalo Vallerino <Gonzalo Vallerino>
2016-03-19 19:33:31 -0300

    Se agrega el test de set con cuantificador +

--- src/test/java/ar/fiuba/tdd/template/tp0/RegExGeneratorTest.java
+++ src/test/java/ar/fiuba/tdd/template/tp0/RegExGeneratorTest.java
@@ -27,7 +27,7 @@ public class RegExGeneratorTest {
                         },
                         (item1, item2) -> item1 && item2);
     }
-    
+
     @Test
     public void testAnyCharacter() {
         assertTrue(validate(".", 1));
@@ -57,12 +57,12 @@ public class RegExGeneratorTest {
     public void testCharacterSet() {
         assertTrue(validate("[abc]", 1));
     }
-    /*
+
     @Test
     public void testCharacterSetWithQuantifiers() {
         assertTrue(validate("[abc]+", 1));
     }
-
+    /*
         @Test
     public void testEnunciado() {
         String regEx = "..+[ab]*d?c";
